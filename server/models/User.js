bae84ade4387182cb2618293eba3498718e75dce
CrazyCoder254 <kentech.softwares@gmail.com>
2025-07-02 23:53:37 +0300

    frontend complete

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,11 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
 
 const userSchema = new mongoose.Schema({
-    email : {type: String, required: true, unique: true},
-    password: {type: String, required: true, },
-    role: { type: String, enum:["admin", "developer"], default:"developer"}
-})
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["developer", "admin"], default: "developer" }
+});
 
 module.exports = mongoose.model("User", userSchema);
